7e1a705efe030c26f7f7873ee79ae9936e87edb3
Kandie Kip <zeddarn@gmail.com>
2022-11-11 19:20:48 -0600

    Clean up (#3)
    * Initial
    
    * Clean up
    
    * Wip

--- src/main/java/com/spiderwalker/chance/util/FileUtils.java
+++ src/main/java/com/spiderwalker/chance/util/FileUtils.java
@@ -2,6 +2,7 @@ package com.spiderwalker.chance.util;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
+import com.spiderwalker.chance.exception.RangeError;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +12,11 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FileUtils {
+    private static final Logger LOGGER = Logger.getLogger(FileUtils.class.getName());
     public static File readFile() {
         File file = null;
         try {
@@ -20,7 +24,7 @@ public class FileUtils {
             String fileName = "data.json";
             file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE,e.getMessage());
         }
 
         return file;
